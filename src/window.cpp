628638b48b746b6f46a087848d9b6756540b5d2a
zhuyongqi9 <zhuyongqi9@gmail.com>
2024-11-13 16:46:23 +0800

    tmp

---
+++ src/window.cpp
@@ -0,0 +1,125 @@
+#include <ftxui/component/component_base.hpp>
+#include <ftxui/component/component_options.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/dom/node.hpp>
+#include <ftxui/dom/table.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/component/component.hpp>
+#include <filesystem>
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <vector>
+
+std::vector<std::string> skipped_file = {
+    "Photos Library.photoslibrary",
+};
+
+bool skipped(std::string &path) {
+    for (auto &item : skipped_file) if (item == path) return true;
+    return false;
+}
+
+std::vector<std::filesystem::directory_entry> search_file(std::string &name, std::filesystem::path &path) {
+    auto file = std::filesystem::recursive_directory_iterator(path, std::filesystem::directory_options::skip_permission_denied);
+    std::vector<std::filesystem::directory_entry> res(0);
+
+    try {
+        for(auto it = file; it != std::filesystem::end(file); it++) {
+            const std::filesystem::directory_entry &entry = *it;
+            std::string file_name = entry.path().filename().string();
+            if (skipped(file_name)) {
+                it.disable_recursion_pending();
+                continue;
+            }
+
+            if (file_name.size() > 0 && file_name[0] == '.') {
+                //fmt::print(fmt::bg(fmt::color::red), file_name);
+                //fmt::print("\n");
+                it.disable_recursion_pending();
+                continue;
+            }
+            //fmt::println(entry.path().string());
+            if (file_name.find(name) == 0) {
+                res.push_back(entry);
+            }
+        }
+    } catch (const std::filesystem::filesystem_error &e) {
+        
+    }
+    return res;
+}
+
+
+std::vector<std::filesystem::directory_entry> list_all(std::filesystem::path path) {
+    auto start = std::filesystem::directory_iterator(path, std::filesystem::directory_options::skip_permission_denied);
+    std::vector<std::filesystem::directory_entry> res;
+    for (auto it = start; it != std::filesystem::end(start); it++) {
+        auto entry = *it;
+        res.push_back(entry);
+    }
+    return res;
+}
+
+using namespace ftxui;
+Component list_view(std::filesystem::path current) {
+    
+}
+
+int main() {
+
+    std::string target_file;
+    Component input_target_file = Input(&target_file, "", InputOption::Default());
+
+    std::filesystem::path current =  std::filesystem::current_path();
+
+
+    auto files = list_all(current);
+    std::vector<std::string> string_view;
+    for (auto &file : files) {
+        string_view.push_back(file.path().filename().string());
+    }
+
+    int selected = 0;
+    MenuOption option;
+    option.on_enter = [&current, &files, &selected]()mutable {
+        std::filesystem::directory_entry selected_entry = files[selected];
+        if (selected_entry.is_directory()) {
+            current = files[selected].path();
+        }
+    };
+    auto entry = Menu(string_view, &selected, option);
+
+    Component container = Container::Vertical({
+    });
+
+    std::vector<Component> fixed_component = {
+        input_target_file
+    };
+
+    auto component = Renderer(container, [&] {
+        container->Add(fixed_component[0]);
+        container->Add(entry);
+        string_view.clear();
+        for (auto &file : files) {
+            string_view.push_back(file.path().filename().string());
+        }
+
+        return gridbox({
+            {
+                text(fmt::format("Current Dir: {}", current.string())) | bold
+            },
+            {
+                window(text("Target File"), input_target_file->Render())
+            }, {
+                entry->Render()
+            } ,{
+                text(fmt::format("{}", selected))
+            }
+        });
+    });
+
+    auto screen = ScreenInteractive::Fullscreen(); 
+    screen.Loop(component);
+
+}
